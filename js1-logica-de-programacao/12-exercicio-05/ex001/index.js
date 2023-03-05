03acdee1745282aed76bbb2cef479973f4312615
Alexandre Retamero Junior <121994672+aleretamero@users.noreply.github.com>
2023-03-05 13:24:22 -0300

    Update index.js

--- js1-logica-de-programacao/12-exercicio-05/ex001/index.js
+++ js1-logica-de-programacao/12-exercicio-05/ex001/index.js
@@ -2,14 +2,31 @@ let opcao;
 
 do {
   opcao = prompt(
-    `Selecione a opção desejada:\n
-    [1]\n
-    [2]\n
-    [3]\n
-    [4]\n
-    [5] Encerrar`
+    `Selecione a opção desejada:
+    \n[1]
+    \n[2]
+    \n[3]
+    \n[4]
+    \n[5] Encerrar`
   );
-  alert(`Opção selecionada → ${opcao}`);
-} while (opcao != 5);
 
-alert("Finalizando operação");
+  switch (opcao) {
+    case "1":
+      alert("Você escolheu a opção 1");
+      break;
+    case "2":
+      alert("Você escolheu a opção 2");
+      break;
+    case "3":
+      alert("Você escolheu a opção 3");
+      break;
+    case "4":
+      alert("Você escolheu a opção 4");
+      break;
+    case "5":
+      alert("Você escolheu encerrar...");
+      break;
+    default:
+      alert("Opção inválida.");
+  }
+} while (opcao != 5);
