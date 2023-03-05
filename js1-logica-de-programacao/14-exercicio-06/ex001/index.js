1184931ac6fb837b12fa9570dd11c44cfc8d469b
Alexandre Retamero Junior <121994672+aleretamero@users.noreply.github.com>
2023-03-05 17:34:09 -0300

    Update index.js

--- js1-logica-de-programacao/14-exercicio-06/ex001/index.js
+++ js1-logica-de-programacao/14-exercicio-06/ex001/index.js
@@ -1,7 +1,7 @@
 const numeroTabuada = Number(prompt("Digite um número para obter sua tabuada"));
 let resultadoTabuada = "";
 
-for (i = 1; i <= 20; i++) {
+for (let i = 1; i <= 20; i++) {
   resultadoTabuada += `${numeroTabuada} X ${i} = ${numeroTabuada * i}\n`;
 }
 
