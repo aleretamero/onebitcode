577d7cbbf18735b5393359aa7be9126973b4e94d
Alexandre Retamero Junior <121994672+aleretamero@users.noreply.github.com>
2023-03-07 11:01:34 -0300

    Create index.js

---
+++ js1-logica-de-programacao/21-funcoes-escopo/index.js
@@ -0,0 +1,32 @@
+let pokemon = "Charmander";
+
+function evoluir() {
+  pokemon = "Charmeleon";
+}
+
+console.log(pokemon);
+evoluir();
+console.log(pokemon);
+/*
+function criarAnimal() {
+  let animal = "Gato";
+}
+criarAnimal();
+console.log(animal);
+*/
+function avaliarNota(nota) {
+  if (nota > 60) {
+    var aprovado = true;
+    let situacao = "Aprovado";
+  } else {
+    var aprovado = false;
+    let situacao = "Reprovado";
+  }
+
+  console.log(nota);
+  console.log(aprovado);
+  //console.log(situacao);
+}
+
+avaliarNota(83);
+avaliarNota(49);
