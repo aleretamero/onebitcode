f58859b2501cfa2731f785b1df218b1c2d87fe85
Alexandre Retamero Junior <121994672+aleretamero@users.noreply.github.com>
2023-03-07 13:21:33 -0300

    Create index.js

---
+++ js1-logica-de-programacao/26-funcoes-high-order/index.js
@@ -0,0 +1,45 @@
+// High-Order Functions é uma função que
+// recebe outra função como parâmetro
+
+function calcular(a, b, operacao) {
+  console.log("Realizando uma operação");
+  const resultado = operacao(a, b);
+  return resultado;
+}
+
+function somar(x, y) {
+  console.log("Realizando uma soma.");
+  return x + y;
+}
+
+console.log(calcular(3, 5, somar));
+
+console.log(
+  calcular(8, 4, function (x, y) {
+    console.log("Realizando uma subtração");
+    return x - y;
+  })
+);
+
+function exibirElemento(elemento, indice, array) {
+  console.log({
+    elemento,
+    indice,
+    array,
+  });
+}
+
+const lista = ["Maça", "Banana", "Laranja", "Limão"];
+
+for (let i = 0; i < lista.length; i++) {
+  exibirElemento(lista[i], i, lista);
+}
+lista.forEach(exibirElemento);
+
+lista.forEach(function (elemento, indice, array) {
+  console.log({
+    elemento,
+    indice,
+    array,
+  });
+});
