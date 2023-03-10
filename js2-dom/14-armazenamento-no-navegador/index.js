d1b02a44517cbda64545b0f273660a18b6a3ee78
Alexandre Retamero Junior <121994672+aleretamero@users.noreply.github.com>
2023-03-10 00:50:10 -0300

    Create index.js

---
+++ js2-dom/14-armazenamento-no-navegador/index.js
@@ -0,0 +1,42 @@
+document.getElementById('sessionBtn').addEventListener('click', function () {
+  const input = document.getElementById('session');
+  sessionStorage.setItem('info', input.value);
+});
+
+document.getElementById('readSession').addEventListener('click', function () {
+  const info = sessionStorage.getItem('info');
+  alert(`A informação salva é: ${info}`);
+});
+
+document.getElementById('localBtn').addEventListener('click', function () {
+  const input = document.getElementById('local');
+  localStorage.setItem('info', input.value);
+});
+
+document.getElementById('readLocal').addEventListener('click', function () {
+  const info = localStorage.getItem('info');
+  alert(`A informação salva é: ${info}`);
+});
+
+document.getElementById('cookieBtn').addEventListener('click', function () {
+  const input = document.getElementById('cookie');
+  // cookieName=value; expires=UTCStringDate; path=/;
+  const cookie = 'info=' + input.value + ';';
+  const expiration = 'expires=' + new Date(2022, 9, 9) + ';';
+  const path = 'path=/;';
+  document.cookie = cookie + expiration + path;
+  input.value = '';
+  console.log(document.cookie);
+});
+
+document.getElementById('cookie2Btn').addEventListener('click', function () {
+  const input = document.getElementById('cookie2');
+  // cookieName=value; expires=UTCStringDate; path=/;
+  const cookie = 'text=' + input.value + ';';
+  const expiration = 'expires=' + new Date(2022, 8, 9) + ';';
+  const path = 'path=/;';
+  document.cookie = cookie + expiration + path;
+  input.value = '';
+  console.log(document.cookie);
+});
+
